b83bb95da0a05b51a6102067966e27c48dca6717
もちとち <35099523+mochitochi@users.noreply.github.com>
2019-09-29 11:10:42 +0900

    Update 2019-algorithm-1.cpp
    decode

--- source/2019-algorithm-1.cpp
+++ source/2019-algorithm-1.cpp
@@ -1,16 +1,16 @@
 int main(){
-    unsigned long long N, Q; cin &gt;&gt; N &gt;&gt; Q;//入力を受けとる
-    vector&lt;int&gt; X(N);
+    unsigned long long N, Q; cin >> N >> Q;//入力を受けとる
+    vector<int> X(N);
 
-    for(int i = 0; i &lt; N; i++)cin &gt;&gt; X[i];//入力を受け取る
-    vector&lt;int&gt; S(N + 1, 0);
+    for(int i = 0; i &lt; N; i++)cin >> X[i];//入力を受け取る
+    vector<int> S(N + 1, 0);
 
     for(int i = 0; i &lt; N; i++){
         S[i + 1] = S[i] + X[i];//ここでワンポイント。一つ前のSの値を再利用することで計算量を削減できる
 
     for(int i = 0; i &lt; Q; i++){
-        unsigned long long l, r; cin &gt;&gt; l &gt;&gt; r;
-        cout &lt;&lt; S[r] - S[l] &lt;&lt; endl;
+        unsigned long long l, r; cin >> l >> r;
+        cout << S[r] - S[l] << endl;
     }
 return 0;
 }
