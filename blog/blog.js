1a156cf780a7b415cda7630147fb08bd0e1c0f38
ocean0801 <87162635+ocean0801@users.noreply.github.com>
2022-06-19 14:44:08 +0900

    general.jsの読み込み先を修正

--- blog/blog.js
+++ blog/blog.js
@@ -8,8 +8,8 @@ window.addEventListener("load", (event) => {
             const block = document.getElementById("blogBlock");
             for (let i = 0;i<pages["pages"].length;i++){
                 let date = pages["pages"][i]["date"].split("/");
-        
-                block.innerHTML += 
+                
+                block.innerHTML +=
                 `
                 <div class="blogPosts">
                     <div class="articleUnit">
@@ -44,6 +44,44 @@ window.addEventListener("load", (event) => {
                         <div class="prev">前へ<a href="${pages["pages"][i+1]["URL"]}">${pages["pages"][i+1]["name"]} &gt;</a></div>
                         `;
                     }
+                }
+            }
+
+            const sidemenu = document.getElementById("sidemenu");
+
+            for (let i = 0;i<pages["pages"].length;i++){
+                if (pages["pages"][i]["URL"] == location.pathname.split("index.html")[0]) {
+                    if (i == 0) { //最新
+                        sidemenu.innerHTML = 
+                        `
+                        <div class="side-back"><a href="../index.html">記事の一覧へもどる<i class="fas fa-sign-out-alt"></i></a></div>
+                        <div class="side-next"><i class="fas fa-angle-left"></i><a href="">Coming Soon...</a></div>
+                        <div class="side-prev"><a href="${pages["pages"][i+1]["URL"]}">${pages["pages"][i+1]["name"]}<i class="fas fa-angle-right"></i></a></div>
+                        `;
+                    } else if (i == pages["pages"].length -1) { //最古
+                        sidemenu.innerHTML = 
+                        `
+                        <div class="side-back"><a href="../index.html">記事の一覧へもどる<i class="fas fa-sign-out-alt"></i></a></div>
+                        <div class="side-prev"><a href="${pages["pages"][i+1]["URL"]}">${pages["pages"][i+1]["name"]}</a><i class="fas fa-angle-right"></i></div>
+                        `;
+                    } else { //中間
+                        sidemenu.innerHTML =
+                        `
+                        <div class="side-back"><a href="../index.html">記事の一覧へもどる<i class="fas fa-sign-out-alt"></i></a></div>
+                        <div class="side-prev"><a href="${pages["pages"][i-1]["URL"]}"><i class="fas fa-angle-left"></i>${pages["pages"][i-1]["name"]}</a></div>
+                        <div class="side-prev"><a href="${pages["pages"][i+1]["URL"]}">${pages["pages"][i+1]["name"]}</a><i class="fas fa-angle-right"></i></div>
+                        `;
+                    }
+                }
+            }
+            for (let i = 0;i<pages["pages"].length;i++){
+                if (pages["pages"][i]["URL"] == location.pathname.split("index.html")[0]) {
+                    document.getElementById("breadcrumbs").innerHTML = 
+                    `
+                    <i class="fas fa-home"></i><a href="../../index.html">ホーム</a>
+                    <i class="fas fa-angle-right"></i><a href="../index.html">ブログ</a>
+                    <i class="fas fa-angle-right"></i><a href="./index.html">${pages["pages"][i]["name"]}</a>
+                    `;
                 }
             }
         }
