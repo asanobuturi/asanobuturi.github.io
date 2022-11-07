1536b68fdcfa89a7f259a9eb7e6496e3b47dbed6
ocean0801 <ocean0801@users.noreply.github.com>
2022-11-07 07:29:54 +0000

    deploy: asanobuturi/asanobuturi@8c7c3f8c5e76eb0f6b7bff0366e1b86d21144cb6

--- general.js
+++ general.js
@@ -1,50 +1,23 @@
-window.addEventListener('load', (event) => {
-    const sidebar =
-    `
-    <input id="nav-input" type="checkbox" class="nav-unshown">
-    <label id="nav-open" for="nav-input">
-        <span class="bar bar1"></span>
-        <span class="bar bar2"></span>
-        <span class="bar bar3"></span>
-        <span class="menu">MENU</span>
-        <span class="close">CLOSE</span>
-    </label>
-    <label id="nav-close" for="nav-input"></label>
-    <div id="nav-content">
-        <a class="nav-link" href="/index.html">ホーム</a>
-        <a class="nav-link" href="/about.html">物理部とは</a>
-        <a class="nav-link" href="/document/index.html">部誌</a>
-        <a class="nav-link" href="/game/index.html">ゲーム</a>
-        <a class="nav-link" href="/blog/index.html">ブログ</a>
-        <a class="nav-link" href="/electronics/index.html">電工の部屋</a>
-        <div class="nav-separator"></div>
-        <a class="nav-link" href="http://d.hatena.ne.jp/apc/">ブログ(外部サイト)</a>
-    </div>
-    `;
+const URLs={
+    "/index.html"              : "^/$|^/index.html$",
+    "/about/index.html"        : "^/about/$",
+    "/document/index.html"     : "^/document/*$",
+    "/game/index.html"         : "^/game/.*$",
+    "/blog/index.html"         : "^/blog/.*$",
+    "/electronic/index.html"  : "^/electronic/.*$",
+};
 
-    document.getElementById("nav-drawer").innerHTML = sidebar;
-
-    const URLs={
-        "/index.html"              : "^/$|^/index.html$",
-        "/about.html"              : "^/about.html$",
-        "/document/index.html"     : "^/document.*$",
-        "/game/index.html"         : "^/game/.*$",
-        "/blog/index.html"         : "^/blog/.*$",
-        "/electronics/index.html"  : "^/electronics/.*$",
-    };
-
-    let path;
-    for (let i of Object.entries(URLs)){
-        if ((new RegExp(i[1])).test(location.pathname)){
-            path=i[0];
-            break;
-        }
+let path;
+for (let i of Object.entries(URLs)){
+    if ((new RegExp(i[1])).test(location.pathname)){
+        path=i[0];
+        break;
     }
-    try {
-        document.querySelector(`div#nav-content a[href="${path}"]`).classList.add("isActive");
-    } catch {
-        console.log("なぞのばしょ");
-    }
+}
 
-    document.getElementsByClassName("copyright")[0].innerHTML = "Copyright &copy; 2015 - 2022 Asano Physics Club All Rights Reversed.";
-});
+
+try {
+    document.querySelector(`div#nav-content a[href="${path}"]`).classList.add("isActive");
+} catch {
+    console.log("Unknown page. :(");
+}
