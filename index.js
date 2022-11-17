639b8dcb3117296d7ea3d3f43423274bfa17b487
ocean0801 <ocean0801@users.noreply.github.com>
2022-11-17 09:06:06 +0000

    deploy: asanobuturi/asanobuturi@bdfc86880746a17caa676c210c6d4b5abd0c2d25

---
+++ index.js
@@ -0,0 +1,19 @@
+function loadTwitterJS() {
+    let ad = document.createElement("script");
+    ad.type = "text/javascript";
+    ad.async = true;
+    ad.src = "https://platform.twitter.com/widgets.js";
+    let sc = document.getElementsByTagName("script")[0];
+    sc.parentNode.insertBefore(ad, sc);
+}
+
+function setTimelineHeight() {
+    let timeline = document.getElementsByClassName("twitter-timeline")[0];
+    let height = document.getElementById("blog-list").offsetHeight;
+    timeline.setAttribute("data-height", height);
+}
+
+window.addEventListener("load", function() {
+    loadTwitterJS();
+    setTimelineHeight();
+});
