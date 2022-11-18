d5094c6def6429588c1919566dea863f1cc80fcf
ocean0801 <ocean0801@users.noreply.github.com>
2022-11-18 11:36:05 +0000

    deploy: asanobuturi/asanobuturi@040967264fcb32ca766288f059047650f4ff7ad0

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function setTimelineHeight() {
     let timeline = document.getElementById("twittertimeline");
     let TwitterArticle = document.getElementsByClassName("twitter-article")[0];
     let height = document.getElementById("blog-list").offsetHeight;
-    timeline.setAttribute("style", `border:none; width:100%; height:${height}px;`);
+    console.log(height);
+    timeline.setAttribute("style", `border:none;width: 100%; height:${height}px;`);
     TwitterArticle.setAttribute("style", `height:${height}px;`);
 }
 
 window.addEventListener("load", function() {
-    console.log("Finished loading.");
     setTimelineHeight();
 });
