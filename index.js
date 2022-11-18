25e0bb73991df775db7c2507fad2a1a1459875d8
ocean0801 <ocean0801@users.noreply.github.com>
2022-11-18 11:38:48 +0000

    deploy: asanobuturi/asanobuturi@3771bd920717936a6115e97af67addefd94dbc0b

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function setTimelineHeight() {
     let timeline = document.getElementById("twittertimeline");
     let TwitterArticle = document.getElementsByClassName("twitter-article")[0];
     let height = document.getElementById("blog-list").offsetHeight;
-    console.log(height);
+    if (height == 0) { // ブログの更新欄が表示されていない、つまり横幅が小さい画面のとき
+        height = 500;
+    }
     timeline.setAttribute("style", `border:none;width: 100%; height:${height}px;`);
     TwitterArticle.setAttribute("style", `height:${height}px;`);
 }
