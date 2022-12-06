b17d8ccb7ac3ed93b5ddd504bb1829b695efff68
ocean0801 <ocean0801@users.noreply.github.com>
2022-12-06 11:17:52 +0000

    deploy: asanobuturi/asanobuturi@bb74190dc249d1a852fd4e3ff84f9d9f67244b53

--- general.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function() {
-    (function(window,document){
-        var lazyCss=document.querySelectorAll('link[rel="subresource"]');
-        for(var i=0,l=lazyCss.length;i<l;i++){
-            lazyCss[i].rel="stylesheet";
-        }
-    })(window,document);
-});
